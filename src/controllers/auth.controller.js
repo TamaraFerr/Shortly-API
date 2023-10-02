5ae58a2fb4cf9bc4de9c7efb90cad38559ad2688
TamaraFerr <tamarablacksmith@hotmail.com>
2023-10-02 15:15:31 -0300

    feat: signUp created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,19 @@
+import bcrypt from "bcrypt"
+import { createUser, getUserByEmail } from "../repositories/user.repository.js"
+
 export async function signUp(req, res) {
-    res.send()
+    const {name, email, password} = req.body
+
+    try {
+        const user = await getUserByEmail(email)
+        if(user.rowCount !== 0) return res.status(409).send({message: "Email já está cadastrado!"})
+
+        const hash = bcrypt.hashSync(password, 10)
+        await createUser(name, email, hash)
+        res.sendStatus(201)
+    } catch(error) {
+        res.status(500).send(error.message)
+    }
 }
 
 export async function signIn(req, res) {
---
+++ src/repositories/user.repository.js
@@ -0,0 +1,9 @@
+import db from "../database/database.connection.js"
+
+export function getUserByEmail(email){
+    return db.query(`SELECT * FROM users WHERE email=$1;`, [email])
+}
+
+export function createUser(name, email, password){
+    return db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, password])
+}
