0ad4fa66ecdff2a48a3a27890575063634bf78c4
TamaraFerr <tamarablacksmith@hotmail.com>
2023-10-02 13:02:37 -0300

    chore: adjust code for deploy on render

--- src/App.js
+++ src/App.js
@@ -9,4 +9,7 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-app.listen(PORT, () => console.log(`--------------- Server running on port ${PORT}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
