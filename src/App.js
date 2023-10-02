4079347ef70e3701bfd6df7802f3df1015749c45
TamaraFerr <tamarablacksmith@hotmail.com>
2023-10-02 13:20:15 -0300

    fix: files changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import router from "./routes/index.routes.js"
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(json());
-app.use(router);
+//app.use(router);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
